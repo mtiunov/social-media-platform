51ae36c91fbd54eeb47f19488829cedd7a1c6238
Mikhail <tiunovmixs@gmail.com>
2025-02-08 20:49:55 +0200

    Implementing adding avatar to a profile

--- accounts/models.py
+++ accounts/models.py
@@ -1,5 +1,14 @@
+import pathlib
+import uuid
+
 from django.contrib.auth.models import User
 from django.db import models
+from django.utils.text import slugify
+
+
+def profile_image_path(instance: "Profile", filename: str) -> pathlib.Path:
+    filename = f"{slugify(instance.user.username)}-{uuid.uuid4()}" + pathlib.Path(filename).suffix
+    return pathlib.Path("upload/avatar/") / pathlib.Path(filename)
 
 
 class Profile(models.Model):
@@ -16,7 +25,7 @@ class Profile(models.Model):
     email = models.EmailField(max_length=255, blank=True)
     location = models.CharField(max_length=255, blank=True)
     gender = models.CharField(max_length=50, choices=GenderChoices)
-    picture = models.ImageField(default="avatar.png", upload_to="")
+    picture = models.ImageField(upload_to=profile_image_path, blank=True, null=True)
     slug = models.SlugField(unique=True, blank=True)
     friends = models.ManyToManyField(User, blank=True, related_name="friends")
     update = models.DateTimeField(auto_now=True)
