c2f47ea45449e2b00bb6d091a29070f9e3cdb55f
Mikhail <tiunovmixs@gmail.com>
2025-02-11 12:43:12 +0200

    Implementation permission to the user app

--- social_media_api/settings.py
+++ social_media_api/settings.py
@@ -125,3 +125,9 @@ MEDIA_URL = "/media/"
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+REST_FRAMEWORK = {
+   "DEFAULT_PERMISSION_CLASSES": [
+       "rest_framework.permissions.IsAuthenticated",
+   ]
+}
--- user/urls.py
+++ user/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from user.views import CreateUserView
+from user.views import CreateUserView, ManageUserView
 
 app_name = "user"
 
 urlpatterns = [
     path("register/", CreateUserView.as_view(), name="register"),
+    path("me/", ManageUserView.as_view(), name="manage"),
 ]
