dbdfb3d4a020a68906d1b9b35f1ba442800b921b
Mikhail <tiunovmixs@gmail.com>
2025-03-15 21:25:45 +0200

    Fixed field user in the Comment model, add comment in the Django Admin

--- comments/models.py
+++ comments/models.py
@@ -1,10 +1,10 @@
-from django.contrib.auth import get_user_model
 from django.db import models
+from accounts.models import Profile
 from posts.models import Post
 
 
 class Comment(models.Model):
-    user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+    user = models.ForeignKey(Profile, on_delete=models.CASCADE)
     post = models.ForeignKey(Post, on_delete=models.CASCADE)
     text = models.TextField(max_length=600)
     update = models.DateTimeField(auto_now=True)
