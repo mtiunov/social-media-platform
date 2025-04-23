793992209f6486ed9f2106016362a75eebf2356d
Mikhail <tiunovmixs@gmail.com>
2025-04-23 15:11:35 +0300

    Rewrote the implementation to ModelViewSet

--- comments/serializers.py
+++ comments/serializers.py
@@ -1,16 +1,29 @@
 from rest_framework import serializers
 from comments.models import Comment
+from interactions.serializers import DetailPostSerializers
+from posts.models import Post
 
 
 class CommentSerializers(serializers.ModelSerializer):
+    post = DetailPostSerializers(read_only=True)
+    post_id = serializers.PrimaryKeyRelatedField(queryset=Post.objects.all(), write_only=True)
     user = serializers.HiddenField(default=serializers.CurrentUserDefault())
+    username = serializers.SerializerMethodField(read_only=True)
 
     class Meta:
         model = Comment
-        fields = ("id", "user", "username", "post", "text", "update", "created")
+        fields = ("id", "user", "username", "post", "post_id", "text", "update", "created")
         read_only_fields = ("id", "post", "created")
-
-    username = serializers.SerializerMethodField(read_only=True)
 
     def get_username(self, comment):
         return comment.user.username
+
+    def create(self, validated_data):
+        post = validated_data.pop("post_id")
+        return Comment.objects.create(post=post, **validated_data)
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        request = self.context.get("request")
+        if request and request.method != "POST":
+            self.fields.pop("post_id", None)
--- comments/views.py
+++ comments/views.py
@@ -1,33 +1,21 @@
 from rest_framework import viewsets, status
 from rest_framework.generics import get_object_or_404
+from rest_framework.permissions import IsAuthenticated, AllowAny
 
 from comments.models import Comment
 from comments.serializers import CommentSerializers
 from rest_framework.response import Response
+from posts.models import Post
 
 
-class CommentViewSet(viewsets.ViewSet):
+class CommentViewSet(viewsets.ModelViewSet):
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializers
 
-    def list(self, request):
-        queryset = Comment.objects.filter(user=request.user)
-        serializer = CommentSerializers(queryset, many=True)
-        return Response(serializer.data)
+    def get_permissions(self):
+        if self.action in ["create", "update", "partial_update", "destroy"]:
+            return [IsAuthenticated()]
+        return [AllowAny()]
 
-    def retrieve(self, request, pk=None):
-        queryset = Comment.objects.filter(user=request.user)
-        comment = get_object_or_404(queryset, pk=pk)
-        serializer = CommentSerializers(comment)
-        return Response(serializer.data)
-
-    def update(self, request, pk=None):
-        queryset = get_object_or_404(Comment.objects.filter(user=request.user), pk=pk)
-        serializer = CommentSerializers(queryset, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    def destroy(self, request, pk=None):
-        queryset = get_object_or_404(Comment.objects.filter(user=request.user), pk=pk)
-        comment = get_object_or_404(queryset, pk=pk)
-        return comment.delete()
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
